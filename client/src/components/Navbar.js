74d25f3e2c8a92c2cacbeb04dafe8cc6ba6ce349
jSUNSH1NEw <weertz.joffrey@protonmail.ch>
2022-10-26 11:10:06 +0000

    ⚡️ testingrepos

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,25 +9,8 @@ export function Navbar() {
 
     return(
         <>
-            <nav>
-                <div className="brand">
-                    <img src={Logo} alt="logo" />
-                </div>
-                <ul>
-                    <li className={location.pathname === "/" ? "active" : ""}>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className={location.pathname === "/mint" ? "active" : ""}>
-                        <Link to="/mint">Mint</Link>
-                    </li>
-                    <li className={location.pathname === "/referral" ? "active" : ""}>
-                        <Link to="/referral">Referral</Link>
-                    </li>
-                    <li className={location.pathname === "/referral" ? "active" : ""}>
-                        <Link to="/dashboard">App</Link>
-                    </li>
-                </ul>
-            </nav>
-        </>
+        
+        
+                    </>
     )
 }
